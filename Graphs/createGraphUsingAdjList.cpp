1b178f99a75c97e4fbcf47eb5cc9a2feceeb1e28
Satakshi Garg <56358125+satakshigarg@users.noreply.github.com>
2021-08-08 04:21:37 +0530

    Create createGraphUsingAdjList.cpp

---
+++ Graphs/createGraphUsingAdjList.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void insertEdge(int source, int dest, int* adj) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void defineGraph(int nodes, int *adj) {
+    vector<int> adj[n+1];
+}
