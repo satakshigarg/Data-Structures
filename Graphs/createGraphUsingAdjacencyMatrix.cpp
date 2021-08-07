41afb56bdd7143a7448a2f63bc23c9c68166002a
Satakshi Garg <56358125+satakshigarg@users.noreply.github.com>
2021-08-08 04:18:44 +0530

    Create createGraphUsingAdjacencyMatrix.cpp

---
+++ Graphs/createGraphUsingAdjacencyMatrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void insertEdge(int source, int dest, int* adj) {
+    adj[u][v] = 1;
+    adj[v][u] = 1;
+}
+
+void defineGraph(int nodes, int *adj) {
+    int adj[n+1][n+1];
+    for (int i = 0; i <= n; i += 1) {
+        for (int j = 0; j <=  n; j += 1) {
+            adj[i][j] = 0;
+        }
+    }
+}
