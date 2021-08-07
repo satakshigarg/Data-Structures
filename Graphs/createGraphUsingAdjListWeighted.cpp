f01c5a683d9e2e6ec4732fd2ba1cfbe9f64e02f4
Satakshi Garg <56358125+satakshigarg@users.noreply.github.com>
2021-08-08 04:22:44 +0530

    Create createGraphUsingAdjListWeighted.cpp

---
+++ Graphs/createGraphUsingAdjListWeighted.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void insertEdge(int source, int dest, int* adj, int val) {
+    adj[source].push_back((dest, val));
+    adj[source].push_back((dest, val));
+}
+
+void defineGraph(int nodes, int *adj) {
+    vector<pair<int, int>> adj[n+1];
+}
